7373dfc55b116e3803765140dc91ed0f6617cbc2
이종원 <jklh0202@naver.com>
2020-03-22 02:51:49 +0900

    error fix

--- exercise/reacEx1/react-tutorial/src/index.js
+++ exercise/reacEx1/react-tutorial/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import CalendarView from './CalendarView';
 import Calendar from 'react-github-contribution-calendar';
 import * as serviceWorker from './serviceWorker';
 
